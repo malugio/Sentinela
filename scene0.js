726cc41ec1c1fd3415078fc20ccb5d7c51044ba6
marialfarac <154386866+marialfarac@users.noreply.github.com>
2026-03-16 14:29:02 +0000

    Animação do jogo

--- scene0.js
+++ scene0.js
@@ -1,150 +1,41 @@
 class scene0 extends Phaser.Scene {
   constructor() {
     super("scene0");
-
-    this.player;
-    this.stars;
-    this.bombs;
-    this.platforms;
-    this.cursors;
-    this.score = 0;
-    this.gameOver = false;
-    this.scoreText;
   }
-
   preload() {
-    this.load.image("sky", "assets/sky.png");
-    this.load.image("ground", "assets/platform.png");
-    this.load.image("star", "assets/star.png");
-    this.load.image("bomb", "assets/bomb.png");
-    this.load.spritesheet("dude", "assets/dude.png", {
-      frameWidth: 32,
-      frameHeight: 48,
+    this.load.spritesheet("alien", "assets/alien.png", {
+      frameWidth: 64,
+      frameHeight: 64,
     });
   }
 
   create() {
-    this.add.image(400, 300, "sky");
-    this.platforms = this.physics.add.staticGroup();
-    this.platforms.create(400, 568, "ground").setScale(2).refreshBody();
-    this.platforms.create(600, 400, "ground");
-    this.platforms.create(50, 250, "ground");
-    this.platforms.create(750, 220, "ground");
-    this.player = this.physics.add.sprite(100, 450, "dude");
-    this.player.setBounce(0.2);
-    this.player.setCollideWorldBounds(true);
-
     this.anims.create({
-      key: "left",
-      frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+      key: "stand-still",
+      frames: this.anims.generateFrameNumbers("alien", { start: 20, end: 20 }),
+      frameRate: 1,
+    });
+    
+    this.anims.create({
+      key: "walk-right",
+      frames: this.anims.generateFrameNumbers("alien", { start: 87, end: 95 }),
       frameRate: 10,
-      repeat: -1,
+      repeat: -1
     });
-
-    this.anims.create({
-      key: "turn",
-      frames: [{ key: "dude", frame: 4 }],
-      frameRate: 20,
-    });
-
-    this.anims.create({
-      key: "right",
-      frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-
-    this.cursors = this.input.keyboard.createCursorKeys();
-
-    this.stars = this.physics.add.group({
-      key: "star",
-      repeat: 11,
-      setXY: { x: 12, y: 0, stepX: 70 },
-    });
-
-    this.stars.children.iterate(function (child) {
-      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-    });
-
-    this.bombs = this.physics.add.group();
-
-    this.scoreText = this.add.text(16, 16, "score: 0", {
-      fontSize: "32px",
-      fill: "#000",
-    });
-
-    this.physics.add.collider(this.player, this.platforms);
-    this.physics.add.collider(this.stars, this.platforms);
-    this.physics.add.collider(this.bombs, this.platforms);
-    this.physics.add.overlap(
-      this.player,
-      this.stars,
-      this.collectStar,
-      null,
-      this,
-    );
-    this.physics.add.collider(
-      this.player,
-      this.bombs,
-      this.hitBomb,
-      null,
-      this,
-    );
-  }
-
-  update() {
-    if (this.gameOver) {
-      return;
-    }
-
-    if (this.cursors.left.isDown) {
-      this.player.setVelocityX(-160);
-
-      this.player.anims.play("left", true);
-    } else if (this.cursors.right.isDown) {
-      this.player.setVelocityX(160);
-
-      this.player.anims.play("right", true);
-    } else {
-      this.player.setVelocityX(0);
-
-      this.player.anims.play("turn");
-    }
-
-    if (this.cursors.up.isDown && this.player.body.touching.down) {
-      this.player.setVelocityY(-330);
-    }
-  }
-
-  collectStar(player, star) {
-    star.disableBody(true, true);
-
-    this.score += 10;
-    this.scoreText.setText("Score: " + this.score);
-
-    if (this.stars.countActive(true) === 0) {
-      this.stars.children.iterate(function (child) {
-        child.enableBody(true, child.x, 0, true, true);
+    
+    this.zombie = this.physics.add
+      .sprite(400, 225, "zombie", 20)
+      .setInteractive()
+      .on("pointerdown", () => {
+        if (this.zombie.body.velocity.x === 0) {
+          this.zombie.play("walk-right");
+          this.zombie.setVelocityX(100);
+        } else {
+          this.zombie.play("stand-still");
+          this.zombie.setVelocityX(0);
+        }
       });
 
-      var x =
-        this.player.x < 400
-          ? Phaser.Math.Between(400, 800)
-          : Phaser.Math.Between(0, 400);
-
-      var bomb = this.bombs.create(x, 16, "bomb");
-      bomb.setBounce(1);
-      bomb.setCollideWorldBounds(true);
-      bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-      bomb.allowGravity = false;
-    }
-  }
-
-  hitBomb(player, bomb) {
-    this.physics.pause();
-    this.player.setTint(0xff0000);
-    this.player.anims.play("turn");
-    this.gameOver = true;
   }
 }
 
