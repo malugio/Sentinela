5d14cea6d7f7274fa0c1f5676580e1f51a88d0bc
marialfarac <154386866+marialfarac@users.noreply.github.com>
2026-03-24 14:01:44 +0000

    Colisão

--- scene0.js
+++ scene0.js
@@ -7,27 +7,22 @@ class scene0 extends Phaser.Scene {
     this.direction = undefined;
     this.money = 0;
     this.timer = 120;
-  
-
   }
 
   preload() {
-    this.load.setPath("assets/")
+    this.load.setPath("assets/");
 
-    this.load.tilemapTiledJSON("marte1," "marte1.json"); 
-    
-    this.load.image("tileset", "Mars Tile_Set.png");
-    
-    this.load.spritesheet ("character", this.alien.png, {
-     frameWidth: 32,
-     frameHeight: 32,
-    });
+    this.load.tilemapTiledJSON("marte1", "marte1.json");
 
-    
-    this.load.spritesheet("alien", "assets/alien.png", {
+    this.load.image("tileset", "mars-tileset.png");
+
+
+    this.load.spritesheet("alien", "alien.png", {
       frameWidth: 64,
       frameHeight: 64,
     });
+
+    this.load.image("tileset", "mars-tileset.png");
 
     this.load.spritesheet("buttons", "assets/buttons.png", {
       frameWidth: 32,
@@ -36,30 +31,81 @@ class scene0 extends Phaser.Scene {
 
     this.load.plugin(
       "rexvirtualjoystickplugin",
-      "./rexvirtualjoystickplugin.min.js",
+      "../rexvirtualjoystickplugin.min.js",
       true,
     );
 
-    this.load.audio("music", "assets/music.mp3");
-    this.load.audio("laser", "assets/laser.mp3");
-  
+    this.load.audio("music", "music.mp3");
+    this.load.audio("laser", "laser.mp3");
   }
 
   create() {
+    this.tilemap = this.make.tilemap({ key: "marte1" });
 
-    this.tilemap = this.make.tilemap({key:"map"});
+    this.tilesetTileset = this.tilemap.addTilesetImage(
+      "mars-tileset",
+      "tileset",
+    );
 
-    this.tilesetTileset = this.tilemap.addTilesetImage("tileset");
+    this.layerCeu = this.tilemap.createLayer("ceu", [this.tilesetTileset]);
+    this.layerSol = this.tilemap.createLayer("sol", [this.tilesetTileset, ,]);
+    this.layerAreia = this.tilemap.createLayer("areia", [this.tilesetTileset]);
+    this.layerChaoEscuro = this.tilemap.createLayer("chaoescuro", [this.tilesetTileset,
+    ]);
+    this.layerRochasPequenas = this.tilemap.createLayer("rochaspequenas", [
+      this.tilesetTileset,
+    ]);
+    this.layerRochasMinusculas = this.tilemap.createLayer("rochasminusculas", [
+      this.tilesetTileset,
+    ]);
 
-    this.layer.ceu = this.tilemap.createLayer("ceu", [thistilesetTileset]);
-  
-  this.laser = this.sound.add
-  ("laser");
-    this.music = this.sound.add
-      ("music", { loop: true }).play();
-    
+    this.layerMontanha2 = this.tilemap.createLayer("montanha2", [
+      this.tilesetTileset,
+    ]);
+    this.layerMontanha1 = this.tilemap.createLayer("montanha1", [
+      this.tilesetTileset,
+    ]);
+    this.layerMontanha2Mpde = this.tilemap.createLayer("montanha2mpde", [
+      this.tilesetTileset,
+    ]);
+    this.layerMontanha1Mpde = this.tilemap.createLayer("montanha1mpde", [
+      this.tilesetTileset,
+    ]);
+    this.layerRochasGrandes = this.tilemap.createLayer("rochasgrandes", [
+      this.tilesetTileset,
+    ]);
+    this.layerRochasR = this.tilemap.createLayer("rochasr", [
+      this.tilesetTileset,
+    ]);
 
-  
+    this.layerRochasMedias2 = this.tilemap.createLayer("rochasmedias2", [
+      this.tilesetTileset,
+    ]);
+    this.layerRochasMedias = this.tilemap.createLayer("rochasmedias", [
+      this.tilesetTileset,
+    ]);
+    this.layerRochaPontudaEsquerda = this.tilemap.createLayer(
+      "rocha-pontuda-esquerda",
+      [this.tilesetTileset],
+    );
+    this.layerRochaPontudaDireita = this.tilemap.createLayer(
+      "rocha-pontuda-direita",
+      [this.tilesetTileset],
+    );
+    this.layerPlataformaRandom = this.tilemap.createLayer("plataformar", [
+      this.tilesetTileset,
+    ]);
+    this.layerPlataforma = this.tilemap.createLayer("plataforma", [
+      this.tilesetTileset,
+    ]);
+
+    this.layerConjunto = this.tilemap.createLayer("conjunto", [
+      this.tilesetTileset,
+    ]);
+
+    this.laser = this.sound.add("laser");
+    this.music = this.sound.add("music", { loop: true }).play();
+
     this.anims.create({
       key: "walk-up",
       frames: this.anims.generateFrameNumbers("alien", { start: 0, end: 7 }),
@@ -89,7 +135,55 @@ class scene0 extends Phaser.Scene {
     });
 
     this.alien = this.physics.add.sprite(400, 225, "alien", 20);
-  
+
+    this.physics.world.setBounds(
+      0,
+      0,
+      this.tilemap.widthInPixels,
+      this.tilemap.heightInPixels,
+    );
+    this.cameras.main.setBounds(
+      0,
+      0,
+      this.tilemap.widthInPixels,
+      this.tilemap.heightInPixels,
+    );
+    this.cameras.main.startFollow(this.alien);
+
+    this.alien.setCollideWorldBounds(true);
+
+    this.layerCeu.setCollisionByProperty({ collides: true });
+    this.physics.add.collider(this.alien, this.layerCeu);
+
+    this.layerAreia.setCollisionByProperty({ collides: true });
+    this.physics.add.collider(this.alien, this.layerAreia);
+
+    this.layerChaoEscuro.setCollisionByProperty({ collides: true });
+    this.physics.add.collider(this.alien, this.layerChaoEscuro);
+
+    this.layerSol.setCollisionByProperty({ collides: true });
+    this.physics.add.collider(this.alien, this.layerSol);
+
+    this.layerRochasPequenas.setCollisionByProperty({ collides: true });
+    this.physics.add.collider(this.alien, this.layerRochasPequenas);
+
+    this.layerRochasMinusculas.setCollisionByProperty({ collides: true });
+    this.physics.add.collider(this.alien, this.layerRochasMinusculas);
+
+    this.layerMontanha2.setCollisionByProperty({ collides: true });
+    this.physics.add.collider(this.alien, this.layerMontanha2);
+
+    this.layerMontanha1.setCollisionByProperty({ collides: true });
+    this.physics.add.collider(this.alien, this.layerMontanha1);
+
+    this.layerPlataforma.setCollisionByProperty({ collides: true });
+    this.physics.add.collider(this.alien, this.layerPlatforma);
+    this.layerPlataforma.forEachTile((tile) => {
+      if (tile.properties.collides) {
+        // left, right, up, down
+        tile.setCollision(false, false, true, false);
+      }
+    });
 
     this.joystick = this.plugins.get("rexvirtualjoystickplugin").add(this, {
       x: 100,
@@ -113,7 +207,7 @@ class scene0 extends Phaser.Scene {
       if (this.joystick.force > 0) {
         this.alien.setVelocity(
           this.direction.x * this.speed,
-          this.direction.y * this.speed
+          this.direction.y * this.speed,
         );
 
         switch (true) {
@@ -135,7 +229,7 @@ class scene0 extends Phaser.Scene {
         this.alien.anims.stop();
       }
     });
-  
+
     this.button = this.add
       .sprite(700, 350, "buttons", 10)
       .setScale(2)
@@ -146,13 +240,11 @@ class scene0 extends Phaser.Scene {
       .on("pointerup", () => {
         this.button.setFrame(10);
         this.money += 10;
-        this.textMoney.setText
-          (`Money: ${this.money}`);
+        this.textMoney.setText(`Money: ${this.money}`);
         this.laser.play();
       });
-    
-    this.textMoney = this.add.text
-      (16, 16, `Money: \${this.money}`, {
+
+    this.textMoney = this.add.text(16, 16, `Money: ${this.money}`, {
       fontSize: "32px",
       fill: "#fff",
     });
